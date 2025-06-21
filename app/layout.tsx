0c32591265305334ec4ef5c1e5834a02514e3593
Vipul Singh <vipulsingh.1404@gmail.com>
2025-06-21 14:16:31 +0530

    favicon added

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,8 +39,8 @@ export const metadata: Metadata = {
   },
 
   icons: {
-    icon: "/icon2.svg",
-    shortcut: "/icon2.svg",
+    icon: "/favicon.ico",
+    shortcut: "/favicon.ico",
   },
 
   manifest: "/site.webmanifest",
